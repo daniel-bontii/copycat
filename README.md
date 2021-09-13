ae61c6e28e74e6839b305e30319b9257d86855ec
Daniel Bontii <daniel.bontii@amalitech.org>
2021-09-13 17:56:10 +0000

    completed copycat project

---
+++ README.md
@@ -0,0 +1,3 @@
+# CopyCat CodeCademy Project
+
+This project uses **React** to build a program that lets users type into a textbox and allows them to visualize the immediate effect that these changes have on the web page.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { CopyCatContainer } from './containers/CopyCatContainer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CopyCatContainer />
+    </div>
+  );
+}
+
+export default App;
